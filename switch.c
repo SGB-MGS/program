a6aeb78dab42bf39fbd95437e362af1bdd5fa7c7
SGB-MGS <sgb-mgs@outlook.com>
2021-03-25 21:28:49 +0800

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<wiringPi.h>
+#define green 0
+#define red 1
+#define switch 2
+void init()
+{
+    wiringPiSetup()
+    pinMode(green,OUTPU);
+    pinMode(red,OUTPUT);
+    pinMode(switch,INPUT);
+}
+
+void led(int signal)
+{
+    digitalWrite(green,HIGH);
+    digitalWrite(red,LOW);
+}
+
+void main()
+{
+    init();
+    while(1)
+    {
+        if(0==digitalRead(switch))
+        {
+            delay(500);
+        }
+    }
+}
