08354fd96e4ac6e4a4eb6626ff3ca5a62262e9f2
SGB-MGS <sgb-mgs@outlook.com>
2021-03-25 11:54:35 +0000

    1

--- test.c
+++ test.c
@@ -1,13 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
+#include<unistd.h>
 void* fun1(void* arg)
 {
     int a;
-    char* name=(char*)arg;
+    char* name1=(char*)arg;
     for(a=0;a<50;a++)
     {
-        printf("%s:%d\n",arg,a);
+        printf("%s:%d	",name1,a);
+    }
+}
+void* fun2(void* arg)
+{
+    int a;
+    char* name1=(char*)arg;
+    for(a=0;a<50;a++)
+    {
+        printf("%s:%d	",name1,a);
     }
 }
 void main(void)
@@ -15,7 +25,7 @@ void main(void)
     pthread_t th1;
     pthread_t th2;
     pthread_create(&th1,0,fun1,"th1");
-    pthread_create(&th2,0,fun1,"th2");
+    pthread_create(&th2,0,fun2,"th2");
     pthread_join(th1,0);
     pthread_join(th2,0);
 }
