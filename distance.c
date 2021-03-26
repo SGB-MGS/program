d17d1cccca714d7be18e16ff6675975bfcaf391f
SGB-MGS <sgb-mgs@outlook.com>
2021-03-26 11:54:54 +0000

    1

--- distance.c
+++ distance.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<wiringPi.h>
 #include<sys/time.h>
-#define echo 0
-#define trig 1
+#define echo 1
+#define trig 0
 void init()
 {
 	wiringPiSetup();
@@ -16,13 +16,16 @@ float test()
 	float t1,t2;
 	digitalWrite(trig,LOW);
 	delayMicroseconds(2);
-	degitalWrite(trig,HIGH);
+	digitalWrite(trig,HIGH);
 	delayMicroseconds(10);
-	while(digitalRead(echo)==1)gettimeofday(&ta1,0);
-	while(digitalRead(echo)==0)gettimeofday(&ta2,0);
+	digitalWrite(trig,LOW);
+	while(!(digitalRead(echo)==1));
+	gettimeofday(&ta1,0);
+	while(!(digitalRead(echo)==0));
+	gettimeofday(&ta2,0);
 	t1=ta1.tv_sec*100000+ta1.tv_usec;
 	t2=ta2.tv_sec*100000+ta2.tv_usec;
-	dis=(t2-t1)/1000000*340/2;
+	dis=(t2-t1)/1000000*34000/2;
 	return dis;
 }
 void main()
@@ -30,7 +33,7 @@ void main()
 	init();
 	while(1)
 	{
-		printf("%f m\n",test());	
+		printf("%f cm\n",test());	
 		delay(300);
 	}
 }
