e096865a208633a7a9400afe1ed5339113b2b0df
SGB-MGS <sgb-mgs@outlook.com>
2021-03-26 15:35:35 +0800

    Update led.c

---
+++ led.c
@@ -0,0 +1,20 @@
+void led(int signal)
+{
+    if(0==signal)
+    {
+        digitalWrite(green,LOW);
+        digitalWrite(red,HIGH);
+    }
+    else
+    {
+        if(1==signal)
+        {
+            digitalWrite(green,HIGH);
+            digitalWrite(red,LOW);
+        }
+        else
+        {
+            printf("error");
+        }
+    }
+}
